e2af0473ec44204945815cbd5564350017a1a179
Daniel Glöde <dgms@MacBook-Air-von-Daniel.fritz.box>
2026-07-02 15:39:35 +0200

    Move API request logic to data fetcher and load API key from dotenv

--- data_fetcher.py
+++ data_fetcher.py
@@ -1,14 +1,21 @@
+import os
+
 import requests
+from dotenv import load_dotenv
 
 
 API_URL = "https://api.api-ninjas.com/v1/animals"
-API_KEY = ""
+
+# Load the variables from the .env file.
+load_dotenv()
+
+# Read the API key from the environment.
+API_KEY = os.getenv("API_KEY")
 
 
 def fetch_data(animal_name):
     """
     Fetches the animals data for the animal 'animal_name'.
-
     Returns: a list of animals, each animal is a dictionary:
     {
         'name': ...,
